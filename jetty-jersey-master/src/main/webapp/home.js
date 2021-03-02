b9df9b867887ac1fcf80d50a0941108d649bda08
Pritie Sougoumar <pritie.1999@gmail.com>
2021-03-03 00:07:40 +0100

    Question 3
    Ajout du pdf

--- jetty-jersey-master/src/main/webapp/home.js
+++ jetty-jersey-master/src/main/webapp/home.js
@@ -5,6 +5,30 @@ function getServerData(url, success){
         url: url
     }).done(success);
 }
+function putServerData(url,success){
+	$.ajax({
+		url: url,
+		type: 'PUT',
+	 }).done(success);
+}
+
+function deleteServerData(url,success){
+	$.ajax({
+		url: url,
+		type: 'DELETE',
+	 }).done(success);
+}
+
+function postServerData(url,success){
+	$.ajax({
+		url: url,
+		type: 'POST',
+		dataType: "json",
+	 }).done(success);
+}
+
+
+
 
 function callDone(result){
 	var templateExample = _.template($('#templateExample').html());
